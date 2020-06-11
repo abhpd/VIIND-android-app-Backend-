50b7773bdcc5e18b5e31b6a6b996f9646d50b2b0
Anustup Mukherjee <60361231+Anustup900@users.noreply.github.com>
2020-06-11 22:31:01 +0530

    updated routes for the login/signup page
    Rest API + node.js and mongo DB fixes

---
+++ src/index.js
@@ -0,0 +1,12 @@
+
+const app = require('./app')
+require('./connection');
+
+async function init() {
+    await app.listen(3000);
+    console.log('Server on Localhost:3000')
+}
+module.exports = app;
+
+init();
+
